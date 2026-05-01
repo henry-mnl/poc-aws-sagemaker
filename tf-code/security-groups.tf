63fc35f13660d6607fdb801a0731609b9262c3ce
henry-mnl <henry@mnltechnology.com>
2026-05-01 17:57:40 +0800

    upload tf-code

--- tf-code/security-groups.tf
+++ tf-code/security-groups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "endpoint-sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #allow all inbound traffic, adjust as needed for better security
+    cidr_blocks = ["0.0.0.0/0"] #allow all inbound traffic. Can be scoped further. May not be needed since it's deployed in private subnets.
   }
   egress {
     from_port   = 0
@@ -15,25 +15,4 @@ resource "aws_security_group" "endpoint-sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"] #allow all outbound traffic, adjust as needed for better security
   }
-}
-
-resource "aws_security_group" "sagemaker-domain-sg" {
-  name        = "sagemaker-domain-sg"
-  description = "Security group for SageMaker Domain"
-  vpc_id      = module.vpc.vpc_id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group_rule" "sagemaker-domain-sg-ingress-allow-intra-sagemaker" {
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  security_group_id        = aws_security_group.sagemaker-domain-sg.id
-  source_security_group_id = aws_security_group.sagemaker-domain-sg.id
 }
