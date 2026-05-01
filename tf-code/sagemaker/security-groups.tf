44502e6b2f74747c4c924eef838351a4775ae4af
henry-mnl <henry@mnltechnology.com>
2026-05-01 17:57:52 +0800

    Upload tf-code

---
+++ tf-code/sagemaker/security-groups.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "sagemaker-domain-sg" {
+  name        = "sagemaker-domain-sg"
+  description = "Security group for SageMaker Domain"
+  vpc_id      = module.vpc.vpc_id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "sagemaker-domain-sg-ingress-allow-intra-sagemaker" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.sagemaker-domain-sg.id
+  source_security_group_id = aws_security_group.sagemaker-domain-sg.id
+}
